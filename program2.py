d95f86a758a384f61ce689708fda904c62a370ae
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-11-11 13:43:59 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,5 +1,23 @@
 def decode_message( s: str, p: str) -> bool:
 
 # write your code here
-  
+    msg_len, pat_len = len(message), len(pattern)
+    dp_table = [[False] * (pat_len + 1) for _ in range(msg_len + 1)]
+    
+    dp_table[0][0] = True
+    
+    for col in range(1, pat_len + 1):
+        if pattern[col - 1] == '*':
+            dp_table[0][col] = dp_table[0][col - 1]
+    
+    for row in range(1, msg_len + 1):
+        for col in range(1, pat_len + 1):
+            if pattern[col - 1] == '*':
+                dp_table[row][col] = dp_table[row][col - 1] or dp_table[row - 1][col]
+            elif pattern[col - 1] == '?':
+                dp_table[row][col] = dp_table[row - 1][col - 1]
+            else:
+                dp_table[row][col] = dp_table[row - 1][col - 1] and message[row - 1] == pattern[col - 1]
+    
+    return dp_table[msg_len][pat_len]
         return False
