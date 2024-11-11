d82eb037e69aa3b5ba574718173271e19ee24d5d
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-11-11 13:26:29 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,5 +2,22 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-
+        if not grid:
+            return 0
+        
+        def dfs(i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] != '1':
+                return
+            grid[i][j] = '0'  # mark as visited
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    num_islands += 1
+                    dfs(i, j)
         return 0
