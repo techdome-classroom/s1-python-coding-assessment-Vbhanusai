6ed80cac4e3cd61a2488e2739f6fca81d38b2509
Vbhanusai <bhanusaiveeramallu@gmail.com>
2024-11-11 13:26:11 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -3,20 +3,5 @@ class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
     #    write your code here
-        if not grid:
-            return 0
-        dir = [(1,0),(-1,0),(0,1),(0,-1)]
-        ans=0
 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=='L':
-                    ans+=1
-                    queue=deque([(i,j)])
-                    while queue:
-                        x,y = queue.popleft()
-                        if 0<=x<len(grid) and 0<=y<len(grid[0]):
-                            grid[x][y]='W'
-                            for dx,dy in dir:
-                                queue.append((x+dx,y+dy))
         return 0
